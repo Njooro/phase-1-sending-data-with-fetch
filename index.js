f6be96ffc79ea8cf4efe97df3188b607eaf737e6
Maxwell <njoromax5@gmail.com>
2023-10-31 14:37:32 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Add your code here
+// setup.js
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({
+        name: name,
+        email: email
+      })
+    })
+      .then(response => response.json())
+      .then(data => {
+        appendToDOM(data.id);
+      })
+      .catch(error => {
+        appendToDOM(error.message);
+      });
+  }
+  
+  // Append the response or error message to the DOM
+  function appendToDOM(message) {
+    const newElement = document.createElement('div');
+    newElement.innerHTML = message;
+    document.body.appendChild(newElement);
+  }
+  
+  module.exports = submitData;
+  
